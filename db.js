b898e97ea9f3b73c0bbd71e51b95a858373fb50e
Diasky <diaskygotah@gmail.com>
2022-04-23 21:06:20 +0200

    init

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+;(async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/dascanioproject',{autoIndex: true})
+        console.log('Connected to MongoDB')
+    } catch (ex) {
+        console.error(`MongoDB connection error:\n${ex}`)
+        process.exit(1)
+    }
+})()
+
+const blogsModel = mongoose.model('blogs', mongoose.Schema({}, {strict: false}))
+
+module.exports = {
+    blogsModel
+}
